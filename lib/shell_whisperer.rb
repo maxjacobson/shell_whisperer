78f40184634421c8814d0b828fff10c06f4d60a7
Max Jacobson <max@hardscrabble.net>
2016-01-06 18:25:02 -0500

    Add a bunch of details to exception message

--- lib/shell_whisperer.rb
+++ lib/shell_whisperer.rb
@@ -2,11 +2,18 @@ require "shell_whisperer/version"
 require "English"
 
 module ShellWhisperer
-  class CommandFailed < StandardError; end
+  class CommandFailed < StandardError
+    def initialize(cmd, result, exit_code)
+      super "Attempted to run #{cmd.inspect} but the shell reported error: " \
+            "#{result.chomp.inspect} and exited with exit code #{exit_code}."
+    end
+  end
 
   def self.run(cmd)
     `#{cmd} 2>&1`.tap do |result|
-      raise CommandFailed, result unless $CHILD_STATUS.success?
+      unless $CHILD_STATUS.success?
+        raise CommandFailed.new(cmd, result, $CHILD_STATUS.exitstatus)
+      end
     end
   end
 end
